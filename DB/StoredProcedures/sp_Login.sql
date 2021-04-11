0a619f8d4f47dd5ed7ed7fdf74afa0719d9e5767
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2021-04-11 20:50:46 +0530

    Added SP to Authenticate Users.
    Signed-off-by: Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>

---
+++ DB/StoredProcedures/sp_Login.sql
@@ -0,0 +1,24 @@
+drop procedure if exists sp_Login;
+create procedure sp_Login(parEmail varchar(255), parPassword varchar(255))
+begin
+    set @isExists = -1;
+    select id
+    into @isExists
+    from tbl_credentials_master
+    where email_address = parEmail
+      and password_hash = parPassword
+      and is_active = 1;
+    if @isExists > 0 then
+        select u.id,
+               u.first_name,
+               u.last_name,
+               u.email_address,
+               u.phone_number
+        from tbl_user_master u
+                 inner join tbl_credentials_master c
+                            on c.email_address = u.email_address
+                                and u.is_active = 1 and c.is_active = 1;
+    else
+        select -1 as id;
+    end if;
+end;
